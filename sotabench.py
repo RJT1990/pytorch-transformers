b8189a48ff380d62cd543e057255171de0549b22
Ross Taylor <rj-taylor@live.co.uk>
2019-08-24 20:43:51 +0100

    Create sotabench.py

---
+++ sotabench.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from pytorch_transformers import GPT2Model, GPT2LMHeadModel, GPT2Tokenizer
+from torchbench.language_modelling import WikiText103
+
+new_model = GPT2Model.from_pretrained('gpt2')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+def model_output_transform(output, target, model):
+    n_embd = 768
+    vocab_size = 50257
+    lm_head = nn.Linear(n_embd, vocab_size, bias=False).cuda()
+    if model.config.torchscript:
+        lm_head.weight = nn.Parameter(transformer.wte.weight.clone())
+    else:
+        lm_head.weight = model.wte.weight
+    hidden_states = output[0]
+    lm_logits = lm_head(hidden_states)
+    return lm_logits
+
+WikiText103.benchmark(
+  model=new_model, 
+  context_length=1024, 
+  encoder=tokenizer, 
+  model_output_transform=model_output_transform)
