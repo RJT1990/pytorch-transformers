dac05b105f062c3a4aae6ec5f58fb2bd8c42652c
Ross Taylor <rj-taylor@live.co.uk>
2019-08-24 23:02:10 +0100

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 from pytorch_transformers import GPT2Model, GPT2LMHeadModel, GPT2Tokenizer
 from torchbench.language_modelling import WikiText103
 
-new_model = GPT2Model.from_pretrained('gpt2')
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+new_model = GPT2Model.from_pretrained('gpt2-large')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2-large')
 
 def model_output_transform(output, target, model):
-    n_embd = 768
+    n_embd = 1280
     vocab_size = 50257
     lm_head = nn.Linear(n_embd, vocab_size, bias=False).cuda()
     if model.config.torchscript:
@@ -22,4 +22,6 @@ WikiText103.benchmark(
   model=new_model, 
   context_length=1024, 
   encoder=tokenizer, 
-  model_output_transform=model_output_transform)
+  model_output_transform=model_output_transform,
+  paper_model_name='GPT-2 Large',
+  paper_pwc_id='language-models-are-unsupervised-multitask')
